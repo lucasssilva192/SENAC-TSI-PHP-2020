82d027f87ca36d48eb4553c32025f4f1be4689ca
lucasss192 <lvcxs10920@gmail.com>
2020-09-01 21:37:46 -0300

    Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    $saldo = 34.5;
+
+    //aspas duplas aceitam string especial
+    echo "Seu saldo é $saldo";
+
+    //sspas simples apenas string pura
+    echo '<br>Seu saldo é $saldo';
+
+    $saldo -= 10; //subtração
+
+    echo "<br><br>Agora seu saldo é $saldo";
+
+    $saldo *= 1000; //multiplicação
+
+    echo "<br><br>Agora seu saldo é $saldo";
